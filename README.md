44d8fffe428ea50ffec92a78f5b4cfa30e0ca343
CalClemmer <cal1296@yahoo.com>
2021-08-16 11:34:51 -0700

    Adding collision, working on aiming

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ProjectOneGame
+==========================================================
+Ideas: 
+1. Tower defense game
+2. Enemies are Shapes 
+    2a. Different shapes can do different things 
+3. Towers fight the shapes. Players place towers. 
+    3a. Towers shoot at shapes. 
+    3b. Ideally, 
+4. Shapes move from left of screen to the right
+    4a. If shapes get all the way to the right player loses 
+
+
+Buglist: 
+1. Returns console error if bullet is spawned on top of enemy triangle 
--- main.js
+++ main.js
@@ -18,6 +18,7 @@ class Triangle {
         this.y = y
         this.color = color
         this.length = length
+        this.speed = speed
         this.alive = true
     }
 
@@ -31,7 +32,7 @@ class Triangle {
         ctx.fillStyle = this.color;
         ctx.fill();
     
-        this.x += 1;
+        this.x += this.speed;
     }
 }
 
@@ -102,15 +103,42 @@ class Bullet {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    triangle = new Triangle(100, 200, '#228B22', 21);
-    turret = new Turret(600, 200, Math.PI*1.75);
+    triangle = new Triangle(100, 200, '#228B22', 21, 1);
+    turret = new Turret(600, 200, Math.PI*.75);
     arrTriangles.push(triangle);
     turret.shoot();
+    spawn5Triangles(0, 200);
+
+// ==========================DEBUGGING===========================
+
+// Create Bullet at Click
+    game.addEventListener("click", function(e) {
+        const bullet = new Bullet(e.offsetX, e.offsetY, 0, 0);
+        arrProjectiles.push(bullet);
+        console.log(arrProjectiles[arrProjectiles.length - 1].y);
+        console.log(arrProjectiles[arrProjectiles.length - 1].x)
+    })
+// Create Triangle at Click
+game.addEventListener("click", function(e) {
+    const triangle = new Triangle(e.offsetX, e.offsetY, 0, 0);
+    arrProjectiles.push(bullet);
+    console.log(arrProjectiles[arrProjectiles.length - 1].y);
+    console.log(arrProjectiles[arrProjectiles.length - 1].x)
+})
+// Fire Gun on B
     document.addEventListener('keydown', function(evt) {
         if (evt.key === 'b') {
           turret.shoot();
         }
     })
+
+// Find Closest Enemy to Turret on F
+    document.addEventListener('keydown', function(evt) {
+        if (evt.key === 'f') {
+        findClosestEnemy(600, 200);
+        }
+    })
+    // this should be 20ms
     const runGame = setInterval(gameLoop, 20);
   })
 
@@ -118,6 +146,8 @@ document.addEventListener('DOMContentLoaded', function() {
   function gameLoop() {
     // Clear the Cavas
     ctx.clearRect(0, 0, game.width, game.height);
+    despawn(arrTriangles);
+    despawn(arrProjectiles);
     arrProjectiles.forEach(element => element.render());
     arrTriangles.forEach(element => element.render());
     turret.render();
@@ -131,9 +161,68 @@ function drawBox(x, y, size, color){
 }
 
 function detectHit() {
+    // triangle collision. Right now they have square collision boxes but that's good enough for now.
+    if (arrProjectiles.length > 0 && arrTriangles.length > 0)
     for (let i = 0; i < arrProjectiles.length; i++) {
         for (let j = 0; j < arrTriangles.length; j++) {
-            
+            if (arrProjectiles[i].x < arrTriangles[j].x + arrTriangles[j].length &&
+                arrProjectiles[i].x > arrTriangles[j].x &&
+                arrProjectiles[i].y > arrTriangles[j].y - 18 &&
+                arrProjectiles[i].y < arrTriangles[j].y + 2
+                ) 
+                {
+                    arrProjectiles.splice(i, 1);
+                    arrTriangles.splice(j, 1);
+                }
         }
     }
 };
+
+function spawn5Triangles(x, y) {
+    for (let i = 0; i < 5; i++) {
+    const triangle = new Triangle(x, y - 100 + (50*i), '#228B22', 21, 1);
+    arrTriangles.push(triangle);
+    }
+}
+
+/*
+function spawnTestTriangle(x, y) {
+    for (let i = 0; i < 5; i++) {
+    const triangle = new Triangle(x, y - 100 + (50*i), '#228B22', 21, 0);
+    arrTriangles.push(triangle);
+    }
+}
+*/
+
+function findClosestEnemy(x, y) {
+    let closestEnemy = [x, y, 0]; // x, y, and distance
+
+    // Make sure there are actually enemies 
+    if (arrTriangles.length === 0 ) {
+        return undefined
+    }
+
+    for (let i = 0; i < arrTriangles.length; i++) {
+        let distance = ((arrTriangles[i].x - x) ** 2 + (arrTriangles[i].y - y) ** 2) ** 0.5;
+        if (distance > closestEnemy[2]) {
+            closestEnemy = [arrTriangles[i].x, arrTriangles[i].y, distance]
+        }
+    }
+    console.log(closestEnemy);
+    return closestEnemy; 
+}
+
+// Gets ride of offscreen items;
+function despawn(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (
+            arr[i].x < -10 || arr[i].x > 900 
+            ||
+            arr[i].y < -10 || arr[i].y > 460
+        ) {
+            arr.splice(i, 1);
+            i--;
+            console.log('Cut')
+        }
+    }
+}
